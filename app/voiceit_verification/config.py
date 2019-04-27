36e2248b7f2c8dac6796e0b46cf8819d7631bfb6
14296721 <Arganaidi.A.S@sberbank.ru>
2019-04-28 01:16:31 +0300

    ID пользователей хранятся в .yaml файле по их именам, функция верефикации зовется по имени

--- app/voiceit_verification/config.py
+++ app/voiceit_verification/config.py
@@ -3,9 +3,4 @@ TOKEN = "tok_7dc04713960b4518b75692d78a601fe9"
 
 LANG = "en-US"
 
-users = {
-   "sasha": "usr_0aca516415ce42659fb86acead2e6ab7",
-   "gosha": "usr_077ec074aaae4b86a638940969811295",
-}
-
 VERIFICATION_PHRASE = "Today is a nice day to go for a walk"
--- app/voiceit_verification/voiceit_wrapper.py
+++ app/voiceit_verification/voiceit_wrapper.py
@@ -1,6 +1,11 @@
 import enum
 import os
+from typing import Optional
+
 from voiceit2 import VoiceIt2
+import yaml
+
+from app.voiceit_verification.config import *
 
 app = VoiceIt2(KEY, TOKEN)
 path = os.path.join(os.path.dirname(os.path.abspath(__file__)))
@@ -23,28 +28,83 @@ def parse_verification_response(response: dict):
     return VerificationStatus.FAIL
 
 
-def verify_user(user: str, file: str) -> VerificationStatus:
+def verify_user(user_name: str, file: str) -> VerificationStatus:
     """
-    Проводит верификацию пользователя по VoiceitID
-    :param user: ID пользователя
+    Проводит верификацию пользователя по имени
+    :param user_name: имя пользователя
     :param file: путь к файлу с записью голоса для верификации
     :return:
     """
+    user_id = get_user_id(user_name)
+    assert user_id
     return parse_verification_response(
-        app.voice_verification(user, LANG, VERIFICATION_PHRASE, file)
+        app.voice_verification(user_id, LANG, VERIFICATION_PHRASE, file)
     )
 
 
-def add_voice_snapshot(user: str, *files: str):
+def add_voice_snapshot(user_name: str, *files: str):
     """
-    Создает слепок для пользователя с VoiceitID
-    :param user: ID пользователя
+    Создает слепок для пользователя по его имени
+    :param user_name: имя пользователя
     :param files: пути к файлам для создания слепка
     :return:
     """
     assert len(files) > 0
+    user_id = get_user_id(user_name)
+    assert user_id
     for file in files:
-        return app.create_voice_enrollment(user, LANG, VERIFICATION_PHRASE, file)
+        app.create_voice_enrollment(user_id, LANG, VERIFICATION_PHRASE, file)
+
+
+def add_new_user(user_name: str):
+    """
+    Создает нового пользователя с указанными именем без слепка
+    :param user_name:
+    :return:
+    """
+    users = yaml.safe_load(open(path + "/users.yaml"))
+    if not users:
+        users = {}
+    if user_name in users:
+        raise KeyError("Этот пользователь уже существует")
+    response = app.create_user()
+    try:
+        user_id = response['userId']
+        with open(path + "/users.yaml", "a") as f:
+            f.write("%s: %s" % (user_name, user_id))
+    except KeyError:
+        print("Не удалось создать пользователя")
+
+
+def delete_user(user_name: str):
+    """
+    Удаляет пользователя по имени
+    :param user_name:
+    :return:
+    """
+    users = yaml.safe_load(open(path + "/users.yaml"))
+    to_drop_id, to_drop_name = None, None
+    if not users:
+        return
+    for _name, _id in users.items():
+        if _name != user_name:
+            pass
+        else:
+            to_drop_name = _name
+            to_drop_id = _id
+    if to_drop_id:
+        del users[to_drop_name]
+        app.delete_user(to_drop_id)
+    with open(path + "/users.yaml", "w") as f:
+        for _name, _id in users.items():
+            f.write("%s: %s" % (_name, _id))
+
+
+def get_user_id(user_name: str) -> Optional[str]:
+    users = yaml.safe_load(open(path + "/users.yaml"))
+    if not users:
+        return
+    return users.get(user_name)
 
 
 def audio(name: str, ext="wav"):
@@ -52,18 +112,15 @@ def audio(name: str, ext="wav"):
 
 
 if __name__ == "__main__":
-    # Creating snapshot
-    files = [audio("sasha.1"), audio("sasha.2"), audio("sasha.3")]
-    result = add_voice_snapshot(users.get("sasha"), *files)
+    delete_user("sasha")
+    add_new_user("sasha")
 
-    # Verifying user
-    # SUCC
+    _dir = "/Users/einstalek/app_server/voiceit_verification/audio/"
+    add_voice_snapshot("sasha",
+                       _dir + "sasha.1.wav",
+                       _dir + "sasha.2.wav",
+                       _dir + "sasha.3.wav")
+
     print(
-        verify_user(users.get("sasha"), path + "/audio/sasha.test.wav")
+        verify_user("sasha", _dir + "sasha.test.wav")
     )
-
-    # FAIL
-    print(
-        verify_user(users.get("sasha"), path + "/audio/gosha.test.wav")
-    )
-
