1f0f6a0d1b65aadabe46854f586b960acbd04ead
Vitaliy Rakitin <vitaliy@MacBook-Pro-SBT.local>
2019-04-27 14:13:13 +0300

    server beautify

--- src/server.py
+++ src/server.py
@@ -10,35 +10,24 @@ def index():
     return render_template("index.html", args=args)
 
 
-@app.route("/speak", methods=["POST"])
-def recognize_google():
+@app.route("/voice", methods=["POST"])
+def recognize():
     try:
         file = request.files["file"]
         if file:
             print("got file")
     except Exception as exception:
-        print("EXCEPTION: ", exception)
+        print("EXCEPTION in recognize: ", exception)
         traceback.print_exc()
 
 
 @app.route("/text", methods=["GET"])
 def get_text():
-    text = request.args.get("text")
-    answer = "ответ"
-    args = {"method": "GET"}
-    
-    return render_template("text.html", question=text, answer=answer, args=args)
-
-# @app.route("/download_results", methods=["GET"])
-# def download_results ():
-#     try:
-#         zip_name = 'wavs.zip'
-#         zip_dir('wavs', zip_name)
-#         zeroize_results()
-#         return send_file(THIS_FILE_PATH +"/"+zip_name, as_attachment=True)
-#     except Exception as e:
-#         print('Exception in download_results: '+ str(e))
-#         return Response(status=500, response = 'Exception in download_results: '+ str(e))
-
-
-
+    try:
+        text = request.args.get("text")
+        answer = "ответ"
+        args = {"method": "GET"}
+        return render_template("text.html", question=text, answer=answer, args=args)
+    except Exception as exception:
+        print("EXCEPTION in get_text: ", exception)
+        traceback.print_exc()
