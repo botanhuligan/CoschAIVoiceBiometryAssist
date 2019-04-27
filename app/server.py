db547be56b87b382e80ab8e43b45b2a82060ede5
Maxim <maxim@ubuntu.ubuntu-domain>
2019-04-28 01:18:30 +0300

    now make register api

--- app/server.py
+++ app/server.py
@@ -16,6 +16,13 @@ active_users = []
 # key - uid, value - response dict
 WAIT_RESPONCES = {}
 
+# key - uid, value -dict
+# { "name" : ""
+#   "words": ['',]
+#   "file_path: ""
+#}
+NEW_USERS = {}
+
 THIS_FILE_PATH = os.path.dirname(os.path.abspath(__file__))
 
 
@@ -54,7 +61,7 @@ def __log_text(uid, msg, answ):
     print("\nUSER:: {0}\nMESSAGE:: {1} \nANSWER:: {2} \n".format(uid, msg, answ))
 
 
-def add_user(uid):
+def __add_user_as_active(uid):
     active_users.append(uid)
     print("USER {0} ADDED".format(uid))
 
@@ -85,20 +92,91 @@ def index():
     return render_template("index.html", args=args)
 
 ### CREATE USER API ###
+@app.route("/add_user/name", methods=["POST"])
+def add_user_api_name():
+    try:
+        name = request.form['name']
+    except Exception as e1:
+        json_content = request.get_json()
+        if json_content is not None:
+            name = json_content.get("name", None)
+        if name is None:
+            name: str = request.args.get('name', default='')
+
+    # get or generate uid
+    uid = str(request.cookies.get('userID'))
+    if uid is None:
+        uid = str(uuid.uuid4())
+        __add_user_as_active(uid)
+
+    new_user = {}
+    new_user['name'] = name
+    NEW_USERS[uid] = new_user
+
+    response = make_response(('OK', 200))
+    response.headers['userID'] = uid
+    return response
+
+@app.route("/add_user/words", methods=["POST"])
+def add_user_api_words():
+    try:
+        words = request.form['words']
+    except Exception as e1:
+        json_content = request.get_json()
+        if json_content is not None:
+            words = json_content.get("words", None)
+        if words is None:
+            words: str = request.args.get('words', default='')
+
+
+@app.route("/add_user/voice", methods=["POST"])
+def add_user_api_voice():
+    try:
+        # get or generate uid
+        uid = str(request.cookies.get('userID'))
+        if uid is None:
+            return redirect(url_for('start'))
+        # save wav file
+        file_name = str(uuid.uuid4()) + '.wav'
+        try:
+            file = request.files["file"]
+            __save_wav(file_name, file.read())
+        except Exception as e1:
+            file = request.data
+            __save_wav(file_name, file)
+
+        # send to recognition
+        recognition_result = __recognize(file_name)
+        if recognition_result is None:
+            voice_file = __save_response_and_get_voice(uid, '', 'Повторите, Вас плохо слышно',
+                                                       'Повторите, Вас плохо слышно')
+        else:
+            answer = bot.message(recognition_result, uid)
+            voice_file = __save_response_and_get_voice(uid, recognition_result, answer, answer)
+
+        return send_file(voice_file, as_attachment=True)
+    except Exception as e:
+        print('Exception in add_user voice_verification: ' + str(e))
+        return Response(status=500, response='Exception in voice handler: ' + str(e))
+
+
 ### CREATE USER API END ###
+
 
 ### RESTORE SESSION API ####
 # start of restore session
 @app.route("/start", methods=["POST"])
 def handle_start():
     uid = str(uuid.uuid4())
-    add_user(uid)
-    is_set_cookie = True
+    __add_user_as_active(uid)
     answer = bot.start(uid)
     voice_file = __save_response_and_get_voice(uid, '', answer, answer)
-    return send_file(voice_file, as_attachment=True)
 
-# get text after voice result
+    response = make_response(send_file(voice_file, as_attachment=True))
+    response.headers['userID'] = uid
+    return response
+
+# get text alwais after voice result
 @app.route("/get_text", methods=["GET"])
 def handle_get_text():
     uid = str(request.cookies.get('userID'))
@@ -152,7 +230,7 @@ def get_text():
             answer = bot.message(text, uid)
         else:
             uid = str(uuid.uuid4())
-            add_user(uid)
+            __add_user_as_active(uid)
             is_set_cookie = True
             answer = bot.start(uid)
         __log_text(uid, text, answer)
